cd7c665d331793b31eb404b4ce78a0073942550b
anonymous-inshadows <abolfazlhosseini0093@gmail.com>
2025-12-25 13:28:03 +0100

    updated the repo

--- main.cpp
+++ main.cpp
@@ -1,94 +1,28 @@
 #include <iostream>
-#include <map>
+#include <array>
 
 using std::cout, std::cin, std::string, std::endl;
 
-int main() {
-	std::map<string, char> table = {{"one", '1'}, {"two", '2'}, {"three", '3'}, {"four", '4'}, {"five", '5'}, {"six", '6'}, {"seven", '7'}, {"eight", '8'}, {"nine", '9'}};
-	char one = table["one"];
-	char two = table["two"];
-	char three = table["three"];
-	char four = table["four"];
-	char five = table["five"];
-	char six = table["six"];
-	char seven = table["seven"];
-	char eight = table["eight"];
-	char nine = table["nine"];
+const char PLAYER_X = 'X';
+const char PLAYER_O = 'O';
 
-	bool playing = true;
-
-	while (playing) {
-		char posx;
-		char posy;
-		cout << "TIC TAC TOE\n";
-		cout << "===========\n";
-		cout << one << '|' << two << '|' << three << '\n';
-		cout << four << '|' << five << '|' << six << '\n';
-		cout << seven << '|' << eight << '|' << nine << '\n';
-
-		cout << "X => Pick a Position to mark: ";
-		cin >> posx;
-		cout << "O => Pick a Position to mark: ";
-		cin >> posy;
-
-		switch (posx) {
-			case '1':
-				one = 'X';
-				break;
-			case '2':
-				two = 'X';
-				break;
-			case '3':
-				three = 'X';
-				break;
-			case '4':
-				four = 'X';
-				break;
-			case '5':
-				five = 'X';
-				break;
-			case '6':
-				six = 'X';
-				break;
-			case '7':
-				seven = 'X';
-				break;
-			case '8':
-				eight = 'X';
-				break;
-			case '9':
-				nine = 'X';
-				break;
-		}
-		switch (posy) {
-			case '1':
-				one = 'O';
-				break;
-			case '2':
-				two = 'O';
-				break;
-			case '3':
-				three = 'O';
-				break;
-			case '4':
-				four = 'O';
-				break;
-			case '5':
-				five = 'O';
-				break;
-			case '6':
-				six = 'O';
-				break;
-			case '7':
-				seven = 'O';
-				break;
-			case '8':
-				eight = 'O';
-				break;
-			case '9':
-				nine = 'O';
-				break;
+void printboard(std::array<char, 9> &board) {
+	cout << "\nTIC TAC TOE\n";
+	cout << "===============\n";
+	for (int i = 0; i < 9; i += 3) {
+		cout << board[i] << " | " << board[i+1] << " | " << board[i+2] << "\n";
+		if (i < 6) {
+			cout << "--+---+--\n";
 		}
 	}
+}
+
+void move(char player) {
+		
+}
+
+int main() {
+	std::array<char, 9> board = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	printboard(board);
 	return 0;
 }
